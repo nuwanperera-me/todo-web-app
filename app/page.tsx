3c8a23918bd01404d49537ef7e2cfe3c7736e8da
Nuwan Perera <nuwanperera613@gmail.com>
2024-05-05 22:03:51 +0530

    feat: add todo card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,69 @@
+'use client';
+
+import ToDoCard from "@/components/todo-card";
+
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return(
-    <div>Hello</div>
+
+  const todoDataArray = [
+    {
+      title: "Do Homework",
+      description: "Todo Description",
+      isImportant: true,
+      date: "2021-10-10",
+    },
+    {
+      title: "Read book",
+      isImportant: true,
+
+      date: "2021-10-10",
+    },
+    {
+      title: "Do Homework",
+      description: "Todo Description",
+      isImportant: false,
+
+      date: "2021-10-10",
+    },
+    {
+      title: "Testing 1",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis corporis, inventore expedita eum quibusdam hic recusandae ullam magni provident ducimus vitae aspernatur. Consequatur at voluptatibus, id quidem aliquid optio rerum?Lorem ipsum dolor ",
+      isImportant: false,
+
+      date: "2021-10-10",
+    },
+    {
+      title: "Do Homework",
+      description: "Todo Description",
+      isImportant: true,
+      date: "2021-10-10",
+    },
+
+  ];
+
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div className="my-6">Hello {session?.user?.name}</div>
+      <button className="fixed w-14 h-14 flex sm:hidden justify-center items-center rounded-full bg-sky-600 bottom-6 right-6">
+        <PlusCircleIcon className="h-8 w-8 text-neutral-50" />
+      </button>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 auto-rows-max gap-4 ">
+        <button className="max-w-screen-sm w-full hidden sm:block mx-auto rounded-lg">
+          <div className="w-full bg-neutral-800 h-48 p-2 rounded-lg">
+            <div className="w-full h-full border-2 border-dashed rounded-sm border-neutral-600 flex items-center justify-center">
+              <PlusCircleIcon className="h-16 w-16 text-neutral-500 dark:text-neutral-600 " />
+            </div>
+          </div>
+        </button>
+        {todoDataArray.map((data) => (
+          <div><ToDoCard title={data.title} description={data.description || ""} isImportant={data.isImportant} date={data.date} /></div>
+        ))}
+      </div>
+    </>
   );
 }
---
+++ components/todo-card.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import {
+  PencilIcon,
+  CheckCircleIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+
+type todoData = {
+  title: string;
+  description?: string;
+  isImportant: boolean;
+  date: string;
+
+};
+
+export default function ToDoCard(data: todoData) {
+  return (
+    <div className="w-full flex flex-col max-w-screen-sm mx-auto p-4 justify-between  bg-white dark:bg-neutral-800 rounded-lg shadow-md">
+      <div className="flex flex-col h-full items-center gap-2 justify-between">
+        <div className="h-full w-full">
+          <div className=" w-full flex justify-between">
+            <h3 className="text-lg font-semibold dark:text-neutral-100">
+              {data.title}
+            </h3>
+            <button className="h-8 rounded-full text-white flex items-center justify-center">
+              <TrashIcon className="h-5 w-5 dark:hover:text-red-500 hover:text-red-600 text-neutral-500 dark:text-neutral-400 transition-colors duration-300" />
+            </button>
+          </div>
+          <div className="text-xs pt-2 font-semibold text-zinc-900 ">
+            {data.isImportant ? <span className=" py-1 px-2 rounded-md text-neutral-50 bg-orange-400">Important</span> : <></>}
+          </div>
+          <p className="text-sm text-neutral-500 dark:text-neutral-400 pt-2">
+            {data.description} 
+          </p>
+        </div>
+        <div className="w-full flex items-center justify-between gap-2">
+          <p className="text-xs text-neutral-400">{data.date}</p>
+
+          <div className="flex gap-2 items-center">
+            <PencilIcon className="h-5 w-5 text-neutral-500 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300" />
+            <button className="rounded-full bg-primary-500 text-zinc-950 dark:text-zinc-50 flex items-center justify-center ">
+              <CheckCircleIcon className="h-6 w-6 text-neutral-500 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
