df6cedf8a641d29457779102f97601f023a4e599
Nuwan Perera <nuwanperera613@gmail.com>
2024-05-06 22:39:20 +0530

    refactor: session object

--- models/user.ts
+++ models/user.ts
@@ -11,10 +11,6 @@ const UserSchema = new Schema({
     required: [true, 'Username is required'],
     match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"],
   },
-  image: {
-    type: String,
-    required: [true, 'Image is required'],
-  }
 });
 
 const User = models.User || model('User', UserSchema);
