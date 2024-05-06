283efb1f09bc35a3bb637275d71714357eb10080
Nuwan Perera <nuwanperera613@gmail.com>
2024-05-07 03:30:26 +0530

    feat: create todo functions

---
+++ app/api/todos/route.ts
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "@/lib/database";
+import Todo from "@/models/todo";
+
+export const POST = async (req: Request) => {
+  const { userId, title, description, isImportant } = await req.json();
+  try {
+    await connectToDatabase();
+    const newTodo = await Todo.create({
+      creater: userId,
+      title,
+      description,
+      isImportant,
+    });
+    console.log(newTodo);
+    await newTodo.save();
+
+    return new Response(JSON.stringify(newTodo), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 500 });
+  }
+};
--- app/create-todo/page.tsx
+++ app/create-todo/page.tsx
@@ -1,9 +1,61 @@
+"use client";
+
 import React from "react";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import Form from "@/components/form";
+
 export default function CreateTodo() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+    isImportant: false,
+  });
+
+  const CreateTodo = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {;
+      const response = await fetch("/api/todos", {
+        method: "POST",
+        body: JSON.stringify({
+          // @ts-ignore
+          userId: session?.user?.id,
+          title: post.title,
+          description: post.description,
+          isImportant: post.isImportant,
+        }),
+      });
+      if (response.ok) {
+        setPost({
+          title: "",
+          description: "",
+          isImportant: false,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+      router.push("/");
+    }
+  };
+
   return (
-    <div>
-      <h1>Create Todo</h1>
+    <div className="mt-10">
+      <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={CreateTodo}
+      />
     </div>
   );
 }
