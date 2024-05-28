25c5d1ac0ce7cf6dc7eaf8846570e3ebd82d9d2c
Nuwan Perera <nuwanperera613@gmail.com>
2024-05-28 23:21:05 +0530

    fix: reload issue

--- components/todo-feed.tsx
+++ components/todo-feed.tsx
@@ -2,11 +2,18 @@
 
 import { useState, useEffect } from "react";
 
-import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import {
+  PencilIcon,
+  CheckCircleIcon,
+  TrashIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
 
-import ToDoCard from "./todo-card";
+// import ToDoCard from "./todo-card";
 import Link from "next/link";
 import { Session } from "next-auth";
+import { cn } from "@/lib/utils";
+import { set } from "mongoose";
 
 interface Todo {
   _id: string;
@@ -17,62 +24,128 @@ interface Todo {
   isDone: boolean;
 }
 
-const ToDoCardList = ({ data }: { data: Todo[] }) => {
-  return (
-    <>
-      {data.map((todo) => (
-        <ToDoCard
-          id={todo._id}
-          title={todo.title}
-          description={todo.description}
-          isImportant={todo.isImportant}
-          date={todo.date.split("T")[0]}
-          isDone={todo.isDone}
-        />
-      ))}
-    </>
-  );
-};
+// const ToDoCardList = ({ data }: { data: Todo[] }) => {
+//   return (
+//     <>
+//       {data.map((todo) => (
+//         <ToDoCard
+//           _id={todo._id}
+//           title={todo.title}
+//           description={todo.description}
+//           isImportant={todo.isImportant}
+//           date={todo.date.split("T")[0]}
+//           isDone={todo.isDone}
+//         />
+//       ))}
+//     </>
+//   );
+// };
 
-const CompletedToDoCardList = ({ data }: { data: Todo[] }) => {
-  return (
-    <>
-      {data.map((todo) => (
-        <ToDoCard
-          id={todo._id}
-          title={todo.title}
-          description={todo.description}
-          isImportant={todo.isImportant}
-          date={todo.date.split("T")[0]}
-          isDone={todo.isDone}
-        />
-      ))}
-    </>
-  );
-};
+// const CompletedToDoCardList = ({ data }: { data: Todo[] }) => {
+//   return (
+//     <>
+//       {data.map((todo) => (
+//         <ToDoCard
+//           _id={todo._id}
+//           title={todo.title}
+//           description={todo.description}
+//           isImportant={todo.isImportant}
+//           date={todo.date.split("T")[0]}
+//           isDone={todo.isDone}
+//         />
+//       ))}
+//     </>
+//   );
+// };
+
+
 
 export default function ToDoFeed(session: Session) {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
-  useEffect(() => {
-    const fetchTodos = async () => {
-      //@ts-ignore
-      const res = await fetch(`/api/todos/user/${session?.user?.id.toString()}`);
-      const data = await res.json();
-      setTodos(data);
-    };
-    fetchTodos();
-  }, [session]);
+  const [isDone, setIsDone] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const handleSubmit = async (e: { preventDefault: () => void }, data: Todo) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch(`/api/todos/${data._id}`, {
+        method: "GET",
+      });
+      if (response.ok) {
+        const todo = await response.json();
+        console.log(todo);
+        todo.isDone = !todo.isDone;
+        await fetch(`/api/complete-todo/${data._id}`, {
+          method: "POST",
+          body: JSON.stringify({
+            userId: todo.creator._id,
+            title: todo.title,
+            description: todo.description,
+            isImportant: todo.isImportant,
+            date: todo.date,
+          }),
+        });
+        await fetch(`/api/todos/${data._id}`, {
+          method: "DELETE",
+          body: JSON.stringify({}),
+        });
+        setIsDone(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const handleDelete = async (e: { preventDefault: () => void }, data: Todo) => {
+    e.preventDefault();
+  
+    try {
+      let response = null;
+      if (data.isDone) {
+        response = await fetch(`/api/complete-todo/${data._id}`, {
+          method: "DELETE",
+          body: JSON.stringify({ id: data._id }),
+        });
+      } else {
+        response = await fetch(`/api/todos/${data._id}`, {
+          method: "GET",
+        });
+      }
+      if (response.ok) {
+        await fetch(`/api/todos/${data._id}`, {
+          method: "DELETE",
+          body: JSON.stringify({}),
+        });
+      }
+      setIsDeleted(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const fetchTodos = async () => {
       //@ts-ignore
-      const res = await fetch(`/api/todos/user/${session?.user?.id.toString()}/completed`);
+      const res = await fetch(`/api/todos/user/${session?.user?.id.toString()}`
+      );
+      const data = await res.json();
+      setTodos(data);
+    };
+    fetchTodos();
+  }, [session, isDone, isDeleted]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      //@ts-ignore
+      const res = await fetch(`/api/todos/user/${session?.user?.id.toString()}/completed`
+      );
       const data = await res.json();
       setCompletedTodos(data);
     };
     fetchTodos();
-  }, [session]);
+  }, [session, isDone, isDeleted]);
 
   return (
     <>
@@ -92,11 +165,137 @@ export default function ToDoFeed(session: Session) {
             </div>
           </div>
         </Link>
-        <ToDoCardList data={todos} />
+        {todos.map((data) => (
+          <div className="w-full flex flex-col max-w-screen-sm mx-auto p-4 justify-between  bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow-md">
+            <div className="flex flex-col h-full items-center gap-2 justify-between">
+              <div className="h-full w-full">
+                <div className=" w-full flex justify-between">
+                  <h3 className="text-lg font-semibold dark:text-neutral-100">
+                    {data.title}
+                  </h3>
+                  <button
+                    onClick={(e) => {
+                      handleDelete(e, data);
+                      setIsDeleted(true);
+                    }}
+                    className="h-8 rounded-full text-white flex items-center justify-center"
+                  >
+                    <TrashIcon className="h-5 w-5 dark:hover:text-red-500 hover:text-red-600 text-neutral-500 dark:text-neutral-400 transition-colors duration-300" />
+                  </button>
+                </div>
+                <div className="text-xs pt-2 font-semibold text-zinc-900 ">
+                  {data.isImportant ? (
+                    <span className=" py-1 px-2 rounded-md text-neutral-50 bg-orange-400">
+                      Important
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <p className="text-sm text-neutral-500 dark:text-neutral-400 pt-2">
+                  {data.description}
+                </p>
+              </div>
+              <div className="w-full flex items-center justify-between gap-2">
+                <p className="text-xs text-neutral-400">{data.date}</p>
+
+                <div
+                  className={cn([
+                    "flex gap-2 items-center",
+                    data.isDone ? "hidden" : "",
+                  ])}
+                >
+                  <Link href={`/edit-todo/${data._id}`}>
+                    <PencilIcon className="h-5 w-5 text-neutral-500 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300" />
+                  </Link>
+                  <button
+                    onClick={(e) => {
+                      handleSubmit(e, data);
+                      setIsDone(true);
+                    }}
+                    className="rounded-full bg-primary-500 text-zinc-950 dark:text-zinc-50 flex items-center justify-center "
+                  >
+                    <CheckCircleIcon
+                      className={cn([
+                        "h-6 w-6 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300",
+                        !data.isDone
+                          ? "text-neutral-500"
+                          : "text-green-500 hover:text-green-400 hover:dark:text-green-400",
+                      ])}
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
       <h2 className="mt-16">Completed Todos</h2>
       <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4 opacity-85">
-        <CompletedToDoCardList data={completedTodos} />
+        {completedTodos.map((data) => (
+          <div className="w-full flex flex-col max-w-screen-sm mx-auto p-4 justify-between  bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow-md">
+            <div className="flex flex-col h-full items-center gap-2 justify-between">
+              <div className="h-full w-full">
+                <div className=" w-full flex justify-between">
+                  <h3 className="text-lg font-semibold dark:text-neutral-100">
+                    {data.title}
+                  </h3>
+                  <button
+                    onClick={(e) => {
+                      handleDelete(e, data);
+                      setIsDeleted(true);
+                    }}
+                    className="h-8 rounded-full text-white flex items-center justify-center"
+                  >
+                    <TrashIcon className="h-5 w-5 dark:hover:text-red-500 hover:text-red-600 text-neutral-500 dark:text-neutral-400 transition-colors duration-300" />
+                  </button>
+                </div>
+                <div className="text-xs pt-2 font-semibold text-zinc-900 ">
+                  {data.isImportant ? (
+                    <span className=" py-1 px-2 rounded-md text-neutral-50 bg-orange-400">
+                      Important
+                    </span>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <p className="text-sm text-neutral-500 dark:text-neutral-400 pt-2">
+                  {data.description}
+                </p>
+              </div>
+              <div className="w-full flex items-center justify-between gap-2">
+                <p className="text-xs text-neutral-400">{data.date}</p>
+
+                <div
+                  className={cn([
+                    "flex gap-2 items-center",
+                    data.isDone ? "hidden" : "",
+                  ])}
+                >
+                  <Link href={`/edit-todo/${data._id}`}>
+                    <PencilIcon className="h-5 w-5 text-neutral-500 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300" />
+                  </Link>
+                  <button
+                    onClick={(e) => {
+                      handleSubmit(e, data);
+                      setIsDone(true);
+                    }}
+                    className="rounded-full bg-primary-500 text-zinc-950 dark:text-zinc-50 flex items-center justify-center "
+                  >
+                    <CheckCircleIcon
+                      className={cn([
+                        "h-6 w-6 hover:text-neutral-950 dark:hover:text-neutral-50 transition-colors duration-300",
+                        !data.isDone
+                          ? "text-neutral-500"
+                          : "text-green-500 hover:text-green-400 hover:dark:text-green-400",
+                      ])}
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
