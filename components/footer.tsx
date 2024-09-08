bd6050252d5b8480acf03823d44eecdc96e63ed3
Nuwan Perera <nuwanperera613@gmail.com>
2024-09-09 01:58:57 +0530

    fix: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full h-32">
-      <div className="flex flex-col justify-center items-center h-full">
+      <div className="flex flex-col justify-center items-center h-full gap-2">
         <p className="text-4xl select-none">🧑🏽‍💻</p>
         <p className="text-sm text-gray-400">
           Imagine by <Link href="https://nuwanperera.me" className="rounded-full">Nuwan Perera</Link>
